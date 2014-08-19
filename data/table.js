3c44360b60a2dd297270b5026d86b987ddf063bd
quinnzhang <quinnxzhang@gmail.com>
2014-08-19 15:46:18 -0500

    commit

--- data/table.js
+++ data/table.js
@@ -1,7 +1,7 @@
-var json_source = JSON.parse(movies);
+var mov = JSON.parse(movies);
 
-    var options = {
-        source: json_source,
+    /*var options = {
+        source: mov,
         rowClass: "classy",
     };
 
@@ -11,4 +11,36 @@ var json_source = JSON.parse(movies);
         json : ['*', 'title', 'rating', 'year', 'length', 'genre', 'url'] // The '*' identity will be incremented at each line
     });
     moviesTable.jsonTableUpdate(options);
-    $("#container").append(moviesTable);
+    $("#container").append(moviesTable);*/
+
+var table = document.getElementById('movTable');
+
+var addRow = function(i){
+    var row = document.createElement('tr');
+    addColumns(row, i);
+    table.appendChild(row);
+};
+
+var addColumns = function(row, i){
+    var title = document.createElement('td');
+    var year = document.createElement('td');
+    var rating = document.createElement('td');
+    var length = document.createElement('td');
+    var genre = document.createElement('td');
+
+    title.innerHTML = mov[i].title;
+    year.innerHTML = mov[i].year;
+    rating.innerHTML = mov[i].rating;
+    length.innerHTML = mov[i].length;
+    genre.innerHTML = mov[i].genre;
+
+    row.appendChild(title);
+    row.appendChild(year);
+    row.appendChild(rating);
+    row.appendChild(length);
+    //row.appendChild(genre);
+};
+
+for (var i = 0; i < 100; i++){
+    addRow(i);
+}
